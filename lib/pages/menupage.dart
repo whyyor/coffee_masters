61771770a32f1d9495ad0693e78cbb0d8d271106
whyyor <whyyor@gmail.com>
2023-05-02 16:29:28 +0530

    add styles to elements in menupage and add two elements

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -6,8 +6,15 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var p = Product(id:1,name:"Dummy Product",price:1.25,image:"");
-    return ProductItem(product: p);
+    var p = Product(id: 1, name: "Dummy Product", price: 1.25, image: "");
+    var q = Product(
+        id: 1, name: "Dummy Product much larger", price: 1.25, image: "");
+    return ListView(
+      children: [
+        ProductItem(product: p),
+        ProductItem(product: q),
+      ],
+    );
   }
 }
 
@@ -23,7 +30,40 @@ class ProductItem extends StatelessWidget {
       padding: EdgeInsets.all(8.0),
       child: Card(
         elevation: 4,
-        child: Text(product.name),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$${product.price}"),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Add"),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
